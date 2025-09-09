c3367aef968b5fa34a39601395cf2e50f5fa99dc
merger <fikihaldiansyah28@gmail.com>
2025-09-09 17:09:06 +0700

    adding operator folder

---
+++ operator/scirpt.js
@@ -0,0 +1,72 @@
+// penjumlahan
+let age = 27;
+age = age + 1;
+console.log(age); 
+
+// pengurangan
+let umur = 30;
+umur = umur - 1;
+console.log(umur);
+
+// perkalian
+let perkalian = 10;
+perkalian = perkalian * 2;
+console.log(perkalian);
+
+// pembagian
+let pembagian = 20;
+pembagian = pembagian / 2;
+console.log(pembagian); 
+
+// modulus
+let sisa = 10;
+sisa = sisa % 3;
+console.log(sisa);
+
+// eksponen
+let pangkat = 2;
+pangkat = pangkat ** 3;
+console.log(pangkat);   
+
+// operator penugasan gabungan
+let x = 5;
+x += 3;
+console.log(x); // x = x + 3
+
+let y = 10;
+y -= 4;
+console.log(y); // y = y - 4
+
+let z = 6;
+z *= 2;
+console.log(z); // z = z * 2    
+
+let a = 20;
+a /= 4;
+console.log(a); // a = a / 4
+let b = 15;
+b %= 4;
+console.log(b); // b = b % 4
+
+let c = 3;
+c **= 2;
+console.log(c); // c = c ** 2   
+
+// operator unary
+let d = 5;
+d++;
+console.log(d); // d = d + 1
+
+let e = 10;
+e--;
+console.log(e); // e = e - 1    
+
+// operator string
+let firstName = "Fikih";
+let lastName = "Aldiansyah";
+let fullName = firstName + " " + lastName;
+console.log(fullName); // fikih Aldiansyah
+
+let greeting = "Hello, ";
+greeting += firstName;
+console.log(greeting); // Hello, Fikih
