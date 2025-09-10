02f474c856eacd67c2ae8ba8df96eaf85b7564af
Fikih aldiansyah <114956596+Merger2@users.noreply.github.com>
2025-09-10 10:53:26 +0700

    Delete variabel directory

--- variabel/script.js
+++
@@ -1,7 +0,0 @@
-const name = "Fikih";
-const age = 23;
-const isstudent = false;
-
-document.getElementById("p1").textContent = `nama anda adalah: ${name}`;
-document.getElementById("p2").textContent = `umur anda adalah: ${age} tahun`;
-document.getElementById("p3").textContent = `status anda adalah: ${isstudent ? "pelajar" : "bukan pelajar"}`;
